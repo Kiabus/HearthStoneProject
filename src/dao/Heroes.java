4ae1fe1bfe1d2687e26870ce0c47cdff4321b997
Kiabus <59321179+Kiabus@users.noreply.github.com>
2022-02-26 18:25:32 +0100

    first dao test

---
+++ src/dao/Heroes.java
@@ -0,0 +1,53 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.sql.SQLException;
+
+import connection.Connessione;
+
+public class Heroes {
+	
+	//implemento la classe connessione
+	private Connection conn = null;
+	private Statement stmn = null;
+	private ResultSet rs = null;
+	
+	public void presentaEroi() throws SQLException{
+		
+		String sql = "select * from eroi";
+		
+		try {
+			//mi connetto al db e creo lo statement
+			conn = Connessione.getConnessione();
+			stmn = conn.createStatement();
+			rs = stmn.executeQuery(sql);
+			
+			while(rs.next()){
+				System.out.println(rs.getString("nome") + " " + rs.getString("cognome") + " della specie " + rs.getString("specie"));
+			}
+			
+		} catch (SQLException e) {
+
+			e.printStackTrace();
+			
+		}
+		finally 
+		{
+			//chiudere connessione
+			conn = Connessione.closeConnessione();
+		}
+			
+	}
+	
+		public static void main(String[] args) throws SQLException {
+			Heroes heroes = new Heroes();
+			heroes.presentaEroi();
+		}
+	
+}
+
+
+
+
